498f5fbebd2c2cb148c4a273af3187478bf3b4ce
Akbar Jafarli <akbaraj@code.edu.az>
2024-05-23 21:15:58 +0400

    Part 3 completed

--- ArrayTask/Program.cs
+++ ArrayTask/Program.cs
@@ -22,33 +22,56 @@
             //======================================================
 
 
-            int[] numbers1 = { 1, 2, 3 };
-            int[] numbers2 = { 9, 10, 11, 12, 13, 14, 15, 16, 1, 2, 3 };
-            bool isExist = false;
-            for (int i = 0; i < numbers1.Length; i++)
+            //int[] numbers1 = { 1, 2, 3 };
+            //int[] numbers2 = { 9, 10, 11, 12, 13, 14, 15, 16, 1, 2, 3 };
+            //bool isExist = false;
+            //for (int i = 0; i < numbers1.Length; i++)
+            //{
+            //    for (int j = 0; j < numbers2.Length; j++)
+            //    {
+            //        if (numbers1[i] == numbers2[j])
+            //        {
+            //            isExist = true;
+            //            break;
+            //        }
+            //        else
+            //        {
+            //            isExist = false;
+            //        }
+
+            //    }
+
+            //}
+            //if (isExist)
+            //{
+            //    Console.WriteLine("Movcuddur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Movcud deyil");
+            //}
+
+            //======================================================
+            //======================================================
+
+
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            bool isDublicate = false;
+            for (int i = 0; i < numbers.Length - 1; i++)
             {
-                for (int j = 0; j < numbers2.Length; j++)
+                if (numbers[i] == numbers[i + 1])
                 {
-                    if (numbers1[i] == numbers2[j])
-                    {
-                        isExist = true;
-                        break;
-                    }
-                    else
-                    {
-                        isExist = false;
-                    }
-
+                    isDublicate = true;
+                    break;
                 }
-
             }
-            if (isExist)
+            if (isDublicate)
             {
-                Console.WriteLine("Movcuddur");
+                Console.WriteLine("Yes");
             }
             else
             {
-                Console.WriteLine("Movcud deyil");
+                Console.WriteLine("No");
             }
 
 
