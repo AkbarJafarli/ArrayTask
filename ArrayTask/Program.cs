5c805b2293410a559c16828fb9e5bece69b62223
Akbar Jafarli <akbaraj@code.edu.az>
2024-05-23 20:11:15 +0400

    Add project files.

---
+++ ArrayTask/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ArrayTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 11, 12, 13, 14, 15, 16, 17, 18, 19, };
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    Console.Write(numbers[i]);
+                    break;
+                }
+                Console.Write(numbers[i] + ",");
+
+            }
+        }
+    }
+}
